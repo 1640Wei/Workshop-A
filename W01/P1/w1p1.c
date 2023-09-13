ea4278aceb90c7ac9bdc43fb034eb548eb852b88
Ching Wei Lai <103228835+1640Wei@users.noreply.github.com>
2023-09-13 12:53:31 -0500

    Add files via upload

---
+++ W01/P1/w1p1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ 
+int main(void)
+{
+   printf("Workshop 1 Part-1\n");
+   printf("=================\n");
+   printf("\n");
+   printf("I'm displaying this using the 'printf' stdio\n");
+   printf("(standard input output) library function!\n");
+   printf("\n");
+   printf("Dear teacher,\n");
+   printf("\n");
+   printf("  I promise I will work hard from this day onward.\n");
+   printf("  I acknowledge that practice is extremely important,\n");
+   printf("  so I will do all workshops, quizzes, and assignments.\n");
+   printf("\n");
+   printf("Sincerely,\n");
+   printf("\n");
+   printf("Ching Wei Lai\n");
+   printf("Student ID# 136893211\n");
+ 
+   return 0;
+}
